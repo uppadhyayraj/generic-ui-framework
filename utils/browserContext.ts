f4c14c8d93de3fcd70d57b94171041ffae5c1385
Raj Uppadhyay <raj.uppadhyay@countdown.co.nz>
2024-12-23 21:17:12 +1300

    added docs to the classes

--- utils/browserContext.ts
+++ utils/browserContext.ts
@@ -1,11 +1,29 @@
 import { chromium, Browser, Page } from 'playwright';
 
+/**
+ * A singleton class that manages the browser and page instances.
+ * 
+ * The `BrowserContext` class provides methods to get and close the singleton instances
+ * of the browser and page. It ensures that only one instance of the browser and page
+ * is created and reused throughout the application.
+ * 
+ * @class BrowserContext
+ * @private
+ * @static
+ */
 class BrowserContext {
   private static browserInstance: Browser | null;
   private static pageInstance: Page | null;
 
   private constructor() {}
 
+  /**
+   * Gets the singleton instance of the browser page.
+   * If the browser instance is not already created, it launches a new browser.
+   * If the page instance is not already created, it opens a new page in the browser.
+   *
+   * @returns {Promise<Page>} A promise that resolves to the browser page instance.
+   */
   static async getInstance(): Promise<Page> {
     if (!BrowserContext.browserInstance) {
         BrowserContext.browserInstance = await chromium.launch();
@@ -16,6 +34,14 @@ class BrowserContext {
     return BrowserContext.pageInstance;
   }
 
+  /**
+   * Closes the current browser and page instances if they exist.
+   * 
+   * This method checks if there are existing instances of the browser and page.
+   * If they exist, it closes them and sets their references to null.
+   * 
+   * @returns {Promise<void>} A promise that resolves when both instances are closed.
+   */
   static async closeInstance() {
     if (BrowserContext.pageInstance) {
         await BrowserContext.pageInstance.close();
--- utils/pageFactory.ts
+++ utils/pageFactory.ts
@@ -1,7 +1,17 @@
 import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/inventoryPage';
 
+/**
+ * A factory class for creating instances of different pages.
+ */
 export class PageFactory {
+  /**
+   * Returns an instance of the requested page.
+   *
+   * @param pageName - The name of the page to retrieve.
+   * @returns An instance of the requested page.
+   * @throws Will throw an error if the page name is not found.
+   */
   static getPage(pageName: string) {
     switch (pageName) {
       case 'LoginPage':
